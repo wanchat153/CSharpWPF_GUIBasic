edd896bd2f1f39c851b07b7c6829e9ba7bf8043c
M153 <wanchatgame@gmail.com>
2023-09-05 21:45:05 +0700

    GUI Basic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace CSharpWPF_GUIBasic
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
